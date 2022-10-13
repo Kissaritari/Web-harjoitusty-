d9199ad66052d5aecbf7cb174db21805f6f22984
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2022-10-13 14:14:48 +0300

    kansioitu

--- skriptit/scripti.js
+++ skriptit/scripti.js
@@ -1,1 +1,10 @@
+"use strict";
 
+function date() {
+    let date = new Date();
+    let paiva = date.getDate();
+    let kk = date.getMonth();
+    let vuosi = date.getFullYear();
+    let pvm = ("Tänään on " + paiva + "." + kk + "." + vuosi);
+    document.getElementById('pvm').innerHTML = pvm;
+}
