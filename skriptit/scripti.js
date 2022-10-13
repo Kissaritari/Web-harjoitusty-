3eddced355ea9bdb38dd64d24db6ae9fae882f20
Kissaritari <97673974+Kissaritari@users.noreply.github.com>
2022-10-13 19:20:13 +0300

    progress

--- skriptit/scripti.js
+++ skriptit/scripti.js
@@ -8,3 +8,17 @@ function date() {
     let pvm = ("Tänään on " + paiva + "." + kk + "." + vuosi);
     document.getElementById('pvm').innerHTML = pvm;
 }
+
+function changePic(source) {
+    document.getElementById('valittu').src=source;
+}
+function changeText() {
+   const statsit = fetch('../media/statsit.json')
+    console.log(statsit)
+
+   /*
+   let nimi = statsit.armorit.armor;
+   let dr = statsit.defenseRating;
+   let dt = statsit.defenseThreshold;
+   document.getElementById('statsit').innerHTML = nimi + "Defense Rating: " + dr + "Defense threshold: " + dt; */
+}
